2913c141cfce57160f04c30adc09a0dc93f947dd
eriseld181 <49815928+eriseld181@users.noreply.github.com>
2021-01-14 23:34:12 +0100

    Sum of two numbers

---
+++ java-exercises/src/SumOfTwoNumbers.java
@@ -0,0 +1,9 @@
+//Build a program that finds the sum of two numbers
+public class SumOfTwoNumbers {
+public static void main(String[] args) {
+	int nr1 =5 ;
+	int nr2 =10;
+	int sum = nr1 + nr2;
+	System.out.println("The sum of "+ nr1 +" + "+nr2+ " = " +sum +".");
+}
+}
